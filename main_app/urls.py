37da890ccb26a66bf1d0ec3a9abb4c7cf2704f78
simone-m-tardigradum <simoneandrewpoe12@yahoo.com>
2020-12-01 18:03:11 -0700

    added about page and route

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,10 @@
+# URL configuration for recycle_yourself
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('about/', views.about, name='about'),
 ]   
     
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+# return HttpResponse('<h1>It works!<h1>')
+
+# --- STATIC PAGES
 def landing(request):
     return render(request, 'landing.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+# --- USER PAGES
+
+# --- POST PAGES
+
+# --- AUTH 
